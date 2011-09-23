9f42ff2f0fcb91900719d2f53dde90509fa1ecce
Evan Beard <beard.evan@gmail.com>
2011-09-22 21:57:33 -0700

    updated bookmarklet code to run only after jquery fully loads. improved instructions in readme

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 
 ## Instructions
 
-1) Go to any photo in the album you want to download. The url should look like
+1) Copy the [bookmarklet js](https://raw.github.com/evanbeard/Facebook-Photo-Downloader/master/bookmarklet.js) 
+into a bookmark (go to bookmarks -> bookmark manager, or equivalent, and copy-paste all of the code into
+the box as if you were making a bookmark for, e.g., google.com.
+2) Go to any photo in the album you want to download. The url should look like
 this "https://www.facebook.com/photo.php?fbid=8532030814&set=a.665432525484.2233540.1310135&type=1".
-You don't want to be in the theatre (remove "&theatre" from the url to get out of it).   
-2) Run the bookmarklet   
-3) Follow instructions printed to the javascript console (you'll use wget to download your images)   
+You don't want to be in the theatre (remove "&theatre" from the url to get out of it).
+3) Run the bookmarklet
+4) Follow instructions printed to the javascript console (you'll use wget to download your images)   
 
 
 ## FAQ
--- bookmarklet.js
+++ bookmarklet.js
@@ -1,57 +1,55 @@
-(function(){
+javascript:(function(){
     // load jquery and don't clobber $ namespace
-    // NB: if copy-pasting this code into the console will need
-    // to paste in and execute the following line before pasting
-    // in the rest of the code
     var s=document.createElement('script');s.src='http://code.jquery.com/jquery-1.5.1.js';document.getElementsByTagName('head')[0].appendChild(s);
-    var jQuery = $.noConflict(true);
-
-    var printInstructions = function() {
-        var message = "Below a list of urls will be" +
-            " printed of all of your images. You should save these" + 
-            " to a file with each URL on a new line and then run" +
-            " \"wget -i download-file-list.txt\". If your files do not" +
-            " all have .jpg extensions you can add .jpg to all files in a directory" +
-            " with this command: \"find . -type f -exec mv '{}' '{}'.jpg \;\"";
-        console.log(message);
-    }
-
-    // set of downloads links already visited
-    var seenDownloadLinks = {};
-    
-    // delay before paging through to the next photo
-    // to download it (adding delay appears more like a human).
-    var CHANGE_PHOTO_DELAY = 1000; // in ms
-    
-    var processImages = function(){
-        // go to next photo (we call a FB js function). 
-        PhotoPermalink.pagerClick("prev")
-        
-        // get second link in the actions box which is the
-        // download link
-        var downloadLink = jQuery(jQuery('#fbPhotoPageActions').children()[1]).attr('href');
-        
-        if(!seenDownloadLinks[downloadLink]) {
-            // haven't seen this image yet
-            seenDownloadLinks[downloadLink] = true;
-            
-            setTimeout(function(){processImages()},
-                       CHANGE_PHOTO_DELAY);
-            
-        } else {
-            // we saw an image we've already seen
-            // so we're finished looping through images
-            printInstructions();
-
-            // print results
-            var result = "";
-            jQuery.each(seenDownloadLinks, function(url, v) { 
-                result += "\n" + url;
-            });
-            console.log(result);
+    s.onload = function() {
+        var jQuery = $.noConflict(true);
+        var printInstructions = function() {
+            var message = "Below a list of urls will be" +
+                " printed of all of your images. You should save these" + 
+                " to a file with each URL on a new line and then run" +
+                " \"wget -i download-file-list.txt\". If your files do not" +
+                " all have .jpg extensions you can add .jpg to all files in a directory" +
+                " with this command: \"find . -type f -exec mv '{}' '{}'.jpg \;\"";
+            console.log(message);
         }
-    }
-    processImages();
 
+        // set of downloads links already visited
+        var seenDownloadLinks = {};
+        
+        // delay before paging through to the next photo
+        // to download it (adding delay appears more like a human).
+        var CHANGE_PHOTO_DELAY = 1000; // in ms
+        
+        var processImages = function(){
+            // go to next photo (we call a FB js function). 
+            PhotoPermalink.pagerClick("prev")
+            
+            // get second link in the actions box which is the download link
+            // Note: the below selector will need to be updated as fb changes
+            // their page
+            var downloadLink = jQuery(jQuery('#fbPhotoPageActions').children()[2]).attr('href');
+            
+            if(!seenDownloadLinks[downloadLink]) {
+                // haven't seen this image yet
+                seenDownloadLinks[downloadLink] = true;
+                
+                setTimeout(function(){processImages()},
+                           CHANGE_PHOTO_DELAY);
+                
+            } else {
+                // we saw an image we've already seen
+                // so we're finished looping through images
+                printInstructions();
+
+                // print results
+                var result = "";
+                jQuery.each(seenDownloadLinks, function(url, v) { 
+                    result += "\n" + url;
+                });
+                console.log(result);
+            }
+        }
+        processImages();
+    }
 })();
 
